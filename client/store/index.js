3e903cfa167820cdb7f19dbe276be6afd3da8835
Jeff Albaral <46907744+jalbaral@users.noreply.github.com>
2022-11-11 13:27:28 -0500

    added newAuth

--- client/store/index.js
+++ client/store/index.js
@@ -2,14 +2,18 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import { createLogger } from "redux-logger";
 import thunkMiddleware from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+// import auth from "../newAuth/reducers/auth";
+// import message from "../newAuth/reducers/message"
 import auth from "./auth";
 import albumsReducer from "./allAlbums";
 import singleAlbumReducer from "./singleAlbum";
 import usersReducer from "./allUsers";
 import singleUserReducer from "./singleUser";
+import message from "../newAuth/reducers/message";
 
 const reducer = combineReducers({
   auth: auth,
+  // message: message,
   albums: albumsReducer,
   singleAlbum: singleAlbumReducer,
   users: usersReducer,
