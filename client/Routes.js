d7563f2850c68c2d7cf21382cbf484b7bc2e7f41
Riley Mack <riley.j.mack@gmail.com>
2022-11-14 16:04:35 -0500

    login final merge-ready

--- client/Routes.js
+++ client/Routes.js
@@ -7,8 +7,7 @@ import { me } from "./store";
 import AllAlbums from "./components/AllAlbums.js";
 import SingleAlbum from "./components/SingleAlbum.js";
 import SingleUser from "./components/User";
-import Login from "./newAuth/components/login.component";
-import Register from "./newAuth/components/register.component";
+import { Login, Signup } from "./components/AuthForm";
 /**
  * COMPONENT
  */
@@ -34,7 +33,7 @@ class Routes extends Component {
           <Switch>
             <Route path="/" exact component={Login} />
             <Route path="/login" component={Login} />
-            <Route path="/signup" component={Register} />
+            <Route path="/signup" component={Signup} />
             <Route path="/home" component={Home} />
             <Route exact path="/albums" component={AllAlbums} />
             <Route exact path="/albums/:albumId" component={SingleAlbum} />
